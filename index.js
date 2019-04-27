c645f12663f259d3915bf36b057b5a37a8eaf626
jimmy <jimmy@JimmydeMacBook-Pro.local>
2019-04-27 16:33:23 +0800

    env variable added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { LineBot } = require('bottender');
 const { createServer } = require('bottender/express');
 const { Line } = require('messaging-api-line');
-const handler = require('./handler/handler.js')
+const handler = require('./handler/handler.js');
+require('dotenv').config()
+
 const bot = new LineBot({
-  // channelSecret: process.env.channelSecret, // 填上 Channel Secret
-  // accessToken: process.env.accessToken // 填上 Access Token
-  accessToken:"Rwf7gygc/VRYx2gZtKDC6Be//WtFaxFSsBnD/SKWN8jW9EMZh48xoZXgbFJatmsc52lt/Gv05yQyw11jM0BBlLLA1jucfkFkYqgjN66x4uJOXhMxSxgHoHlLVQp6tW3o85LJadpv60IhIir+4MRo4gdB04t89/1O/w1cDnyilFU=",
-  channelSecret:"4dbdf03bc69dd166eed787ed2f5d04c9"
+  channelSecret: process.env.Channel_secret,
+  accessToken: process.env.Channel_token
 });
 
 // LISTEN TO ALL THE EVENT
